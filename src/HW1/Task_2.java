9d9638386707d4151b00081c0d8ebba0a5ed93b9
Artsiom Zagvozdzin <zagvozdin.artsiom@gmail.com>
2020-07-07 21:29:18 +0300

    Task_2

--- src/HW1/Task_2.java
+++ src/HW1/Task_2.java
@@ -5,7 +5,7 @@ public class Task_2 {
         int a = 7;
         int b = 6;
         int c = 1;
-        if ((a + b > c) && (b + c > a) && (a + c > b)) {
+        if ((a + b != c) && (b + c != a) && (a + c != b)) {
             System.out.println("Такой треугольник существует");
         } else {
             System.out.println ("Такого треугольника не существует");
