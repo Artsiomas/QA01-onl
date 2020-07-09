b90cb2ecfdeafe962273493b6798a001c88994c5
Artsiom Zagvozdzin <zagvozdin.artsiom@gmail.com>
2020-07-09 19:13:33 +0300

    Task_2

--- src/HW1/Task_2.java
+++ src/HW1/Task_2.java
@@ -2,10 +2,10 @@ package HW1;
 
 public class Task_2 {
     public static void main (String [] args) {
-        int a = 7;
-        int b = 6;
-        int c = 1;
-        if ((a + b != c) && (b + c != a) && (a + c != b)) {
+        int a = 1;
+        int b = 3;
+        int c = 99;
+        if ((a + b > c) && (b + c > a) && (a + c > b)) {
             System.out.println("Такой треугольник существует");
         } else {
             System.out.println ("Такого треугольника не существует");
