7bf947f398201067bcb3049cc63bebec128e18e1
Artsiom Zagvozdzin <zagvozdin.artsiom@gmail.com>
2020-07-12 21:16:34 +0300

    HW2

--- src/HW2/Task_8.java
+++ src/HW2/Task_8.java
@@ -2,12 +2,20 @@ package HW2;
 
 public class Task_8 {
     public static void main (String[] args) {
-        int n = 5;
+        int n = 4;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                    System.out.print("+ ");
+            for (int j = 0; j < n; j++) {
+                if ( j<i ){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("+");
+                }
             }
             System.out.println();
         }
+
+
+
+
     }
 }
